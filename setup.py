422629bb3ce9074146dc0d56a89efef011d2d5b6
ahbuss <abuss@nps.edu>
2018-08-25 15:22:29 -0700

    Fixed a few more variable names to Python-esque (view Java)

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='DESpy',
-    version='0.1.1',
+    version='0.1.4',
     packages=['simkit', 'examples', 'examples.run'],
     url='https://github.com/ahbuss/SimPyKit',
     license='Apache 2.0 License',
     author='Arnold Buss',
     author_email='abuss@nps.edu',
-    description='Support for DES Modeling using Event Graphs'
+    description='Support for DES Modeling using Event Graphs',
+    install_requires=['sortedcontainers']
 )
