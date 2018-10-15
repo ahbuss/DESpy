034a6944e320f2bde0686134cab0a29f6db2d9a2
Buss, Arnold <abuss@nps.edu>
2018-10-15 16:38:34 -0700

    removed build, dist, etc

--- build/lib/simkit/simutil.py
+++
@@ -1,6 +0,0 @@
-from simkit.base import StateChangeListener
-
-class SimpleStateChangeDumper(StateChangeListener):
-
-    def state_change(self, state_change_event):
-        print(str(state_change_event))
