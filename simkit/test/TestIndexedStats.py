ce149ae5581017fc3df85971da9f73f6156fb300
ahbuss <abuss@nps.edu>
2024-04-24 16:36:14 -0700

    fixed test of indexed stats

--- simkit/test/TestIndexedStats.py
+++ simkit/test/TestIndexedStats.py
@@ -12,7 +12,7 @@ rv = RandomVariate.instance('Exponential', mean=2.3)
 stats = IndexedSimpleStatsTally(name)
 for i in range(1000):
     for j in range(4):
-        event = IndexedStateChangeEvent(j, source, name, rv.generate)
+        event = IndexedStateChangeEvent(j, source, name, rv.generate())
         stats.state_change(event)
 
 print(stats)
